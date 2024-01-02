accccea0edf2ad90838f064fee5bdfe87cf5db6d
Muruvvetbati <batimuruvvet@gmail.com>
2024-01-02 17:15:26 +0300

    Initial Commit

---
+++ SwiftDependencyInjc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftDependencyInjc
+//
+//  Created by Devmuruvvet on 2.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
